09c52c9a2d3d056e8148563baaaa27f842b3fd9f
Оксана Кулакова <oksana5566@yandex.ru>
2021-09-29 12:00:16 +0500

    190569 / Интеграция с внешним api салонов -Исправление

--- app/Services/SalonsClientService.php
+++ app/Services/SalonsClientService.php
@@ -20,7 +20,7 @@ class SalonsClientService
     public function getRandomSalons()
     {
         try {
-            $response = $this->client->get('http://studentsapi.academy.qsoft.ru/api/v1/salons?limit=2&in_random_order')->getBody()->getContents();
+            $response = $this->client->get('https://studentsapi.academy.qsoft.ru/api/v1/salons?limit=2&in_random_order')->getBody()->getContents();
             return json_decode($response);
             
         } catch (ClientException $e) {
@@ -30,7 +30,7 @@ class SalonsClientService
 
     public function getSalons()
     {
-        $response = $this->client->get('http://studentsapi.academy.qsoft.ru/api/v1/salons')->getBody()->getContents();
+        $response = $this->client->get('https://studentsapi.academy.qsoft.ru/api/v1/salons')->getBody()->getContents();
         return json_decode($response);
     }
 }
